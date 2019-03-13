1a8ae5c2c5099c230a9b2eaf52eb96faed616bc4
Mika Saari <mika.saari@tut.fi>
2019-03-13 10:36:47 +0200

    Toimii.

--- LightSensor/app/src/main/java/fi/tuni/miksa/lightsensor/MainActivity.java
+++ LightSensor/app/src/main/java/fi/tuni/miksa/lightsensor/MainActivity.java
@@ -1,13 +1,77 @@
 package fi.tuni.miksa.lightsensor;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    private final String TAG="Miksan softa";
 
+    private SensorManager mSensorManager;
+    private Sensor mSensorLight;
+
+    private TextView mTextSensorLight;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d(TAG,"alkoi");
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        mTextSensorLight = (TextView) findViewById(R.id.label_light);
+
+        mSensorLight = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+
+        String sensor_error = getResources().getString(R.string.error_no_sensor);
+
+        if (mSensorLight == null) {
+            mTextSensorLight.setText(sensor_error);
+        }
+    }
+
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        int sensorType = event.sensor.getType();
+        Log.d(TAG,"sensortype "+sensorType);
+        float currentValue = event.values[0];
+        Log.d(TAG,"sensorvalue "+currentValue);
+        switch (sensorType) {
+            // Event came from the light sensor.
+            case Sensor.TYPE_LIGHT:
+                // Handle light sensor
+                mTextSensorLight.setText(getResources().getString(R.string.label_light, currentValue));
+                break;
+            default:
+                // do nothing
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (mSensorLight != null) {
+            mSensorManager.registerListener(this, mSensorLight,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mSensorManager.unregisterListener(this);
     }
 }
